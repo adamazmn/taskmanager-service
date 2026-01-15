8c4b30179db95cb19e233b7d1eaf55b28853fd5c
adam20031 <adamazman193@gmail.com>
2026-01-16 02:20:32 +0800

    add uuid

--- src/main/java/com/example/taskmanager/service/UserTaskService.java
+++ src/main/java/com/example/taskmanager/service/UserTaskService.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Slf4j
